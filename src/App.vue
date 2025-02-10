b749691ba4f37fef3c3b2f36fbd0ed4de1d33de9
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-02-10 14:44:14 +0000

    Ready

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-// import { ref } from 'vue';
-// import Componente from './components/Componente.vue';
+import { ref } from 'vue';
+import Componente from './components/Componente.vue';
 
-// const prueba = ref('hola');
 </script>
 
 <template>
-  <!-- <Componente v-model:prueba="prueba" />
-  <p>{{ prueba }}</p> -->
+  <Componente />
+
 </template>
 
 <style scoped></style>
--- src/components/Componente.vue
+++ src/components/Componente.vue
@@ -1,18 +1,11 @@
 <script setup>
-// import { ref, defineProps, defineEmits, watch } from 'vue';
+import { ref, defineProps, defineEmits } from 'vue';
 
-// const props = defineProps(['prueba']);
-// const emit = defineEmits(['update:prueba']);
-
-// const prueba = ref(props.prueba);
-// watch(prueba, (value) => {
-//   emit('update:prueba', value);
-// });
 
 </script>
 
 <template>
-  <!-- <p>prueba: <input type="text" v-model="prueba"></p> -->
+
 </template>
 
 <style scoped>
