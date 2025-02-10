dc193250dca63be419039bdc2abce6d848110218
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-02-10 14:41:46 +0000

    ayudita

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+// import { ref } from 'vue';
+// import Componente from './components/Componente.vue';
+
+// const prueba = ref('hola');
 </script>
 
 <template>
-
+  <!-- <Componente v-model:prueba="prueba" />
+  <p>{{ prueba }}</p> -->
 </template>
 
 <style scoped></style>
